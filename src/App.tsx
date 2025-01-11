3c6806c4337ea4ee6f9efd290f8e1ade52ef450d
RabeaerisFracto <flaco426@gmail.com>
2025-01-11 14:45:03 -0300

    HornetExtension 0.1.1 -Se añaden comentarios describiendo procesos. -Se prueba cambio de color de fondo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 
-import './App.css'
+import './App.css' //Estilo de opciones de extension (boton de navegador).
 
 function App() {
-  const onClick = async ()=>{
+  const onClick = async ()=>{  //funcion asincrona
     let [tab] = await chrome.tabs.query({active: true});
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id!},
+    //variable [destructurada, primera pestaña activa] = llamada de API de chrome que retorna pestañas activas de navegador.
+    chrome.scripting.executeScript({ // Permite inyectar script de JS en pagina abierta de navegador.
+      target: {tabId: tab.id!}, // targetea la pestaña activa.
       func: () => {
         console.log('Cambio de tema');
         alert('Tema Hornet Black');
-        // (document.querySelector('.xjbqb8w ') as HTMLElement)!.style.backgroundColor = 'red';
-        (document.body as HTMLElement)!.style.backgroundColor = '#000';
+        // (document.querySelector('.xjbqb8w ') as HTMLElement)!.style.backgroundColor = 'red';// querySelector busca clase de elemento en pagina abierta.
+        (document.body as HTMLElement)!.style.backgroundColor = '#000';// Afecta todo el body.
 
         (document.body as HTMLElement)!.style.backgroundImage = 'url(https://wallpapercave.com/wp/wp4377294.png)';
-        (document.body as HTMLElement)!.style.backgroundRepeat = 'no-repeat';
+        (document.documentElement as HTMLElement)!.style.backgroundColor = 'black'; (document.body as HTMLElement)!.style.backgroundRepeat = 'no-repeat';
         (document.body as HTMLElement)!.style.backgroundPosition = 'right';
         (document.body as HTMLElement)!.style.backgroundSize = '90%';
         (document.body as HTMLElement)!.style.overflow = 'hidden';
 
+        // (document.querySelector('.label-menu h1') as HTMLElement)!.textContent = '#555';
+        // Para cambiar contenido de texto seleccionado en pestaña abierta.
 
         (document.querySelector('.esquina-usuario') as HTMLElement)!.style.background = '#000';
         // (document.querySelector('.esquina-usuario') as HTMLElement)!.style.outline = '#fff';
@@ -26,7 +29,7 @@ function App() {
         (document.querySelector('.barra-menu') as HTMLElement)!.style.background = '#000';
         (document.querySelectorAll('.barra-menu button') as NodeListOf<HTMLElement>).forEach(button => {
           button.style.color = '#fff';
-        });
+        });//Para cambiar todos los botones.
         (document.querySelector('.tabla') as HTMLElement)!.style.top = '25px';
 
         // if((document.querySelector('.barra-menu') as HTMLElement)?.style.display !== 'hidden'){
